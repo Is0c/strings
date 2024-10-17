71bd777e8d7b42208640044fd6074435ac4a221a
Is0c <isaacpazb08@gmail.com>
2024-10-17 08:40:06 -0300

    Add files via upload

---
+++ 2bgammed.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string.h>
+#include <conio.h>
+using namespace std;
+int main () {
+	cout<<"ingrese una palabra:";
+	string plb1;      cin>>plb1;
+	cout<<"el tamaño es:"<<plb1.length();
+	cout<<"ingrese segunda palabra:";
+	cin>>plb1;
+	cout<<"el tamaño es:"<<plb1.length();
+	return 0;
+}
+
+
